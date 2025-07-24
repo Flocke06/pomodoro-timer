f365f132d515d235f0036ee3365ed0129e9ea0d1
Flo <flo@MacBookPro.fritz.box>
2025-07-24 23:16:23 +0200

    Anpassung der Skalierungen für Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -68,18 +68,29 @@ export default function App() {
     <>
       <div className="font-mono text-white flex flex-col justify-center items-center sm:w-full w-[100%] h-screen">
 
-        <div className="border sm:px-11 sm:py-14 px-6 py-2 rounded-full sm:border-5 border-4">
+        <div className="border sm:px-10 sm:py-19 px-7 py-2 rounded-full sm:border-4 border-4">
 
-          <div className="flex flex-row sm:mt-17 sm:mb-8 mt-20 mb-2">
-            <button ref={ref} onClick={() => { setTime(1500) }} className="text-sm focus:outline-none focus:ring-0 select-none border rounded-full sm:py-1 sm:px-3 py-1 px-1 hover:bg-neutral-700 focus:bg-neutral-600 border-2">pomodoro</button>
-            <button onClick={() => { setTime(300) }} className="text-sm select-none border rounded-full sm:py-3 sm:px-4 p hover:bg-neutral-700 focus:bg-neutral-600 sm:mx-7 mx-2 px-1 border-2">short break</button>
-            <button onClick={() => { setTime(900) }} className="text-sm select-none border rounded-full sm:py-1 sm:px-3 py-1 px-2 hover:bg-neutral-700 focus:bg-neutral-600 border-2">long break</button>
+          <div className="flex flex-col sm:flex-row sm:gap-4 gap-4 items-center justify-center sm:mt-16 sm:mb-8 mt-5 mb-2">
+            <button
+              ref={ref}
+              onClick={() => { setTime(1500) }}
+              className="text-m sm:text-xl focus:outline-none focus:ring-0 select-none border rounded-full sm:py-4 sm:px-4 py-1 px-2 hover:bg-neutral-700 focus:bg-neutral-600 border-2">pomodoro</button>
+
+            <div className="flex flex-row gap-4 sm:mr-0 mr-3 sm:gap-4">
+              <button
+                onClick={() => { setTime(300) }}
+                className="text-m sm:text-xl select-none border rounded-full sm:py-4 sm:px-4 py-1 px-2 hover:bg-neutral-700 focus:bg-neutral-600 border-2">short break</button>
+              <button
+                onClick={() => { setTime(900) }}
+                className="text-m sm:text-xl select-none border rounded-full sm:py-4 sm:px-4 py-1 px-2 hover:bg-neutral-700 focus:bg-neutral-600 border-2">long break</button>
+            </div>
+
           </div>
 
-          <div className="flex flex-col justify-center w-full items-center text-center mt-5">
+          <div className="flex flex-col justify-center w-full items-center text-center sm:mt-10 mt-4">
             <label className="sm:text-9xl text-8xl">{formatTime(time)}</label>
             <button>
-              <img src={on ? "/assets/icons/pause.svg" : "/assets/icons/play.svg"} alt="test" className="w-25 h-25 focus:outline-none focus:ring-0 select-none mt-2"
+              <img src={on ? "/assets/icons/pause.svg" : "/assets/icons/play.svg"} alt="test" className="w-25 h-25 focus:outline-none focus:ring-0 select-none mt-2 sm:mt-6"
                 onClick={on ? clickPause : clickStart}
               />
             </button>
