36dd492634d81d8cf40e29c80471c65746bf7b5a
Flo <flo@MacBook-Pro-von-Flo.fritz.box>
2025-07-24 22:19:03 +0200

    Bug Fixes, Skalierung angepasst und Logische Abläufe erstellt

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 export default function App() {
   const [time, setTime] = useState(1500);
   const [on, setOn] = useState(false);
+  const [flag, setFlag] = useState(false);
 
   const ref = useRef(null);
 
@@ -16,23 +17,31 @@ export default function App() {
     ref.current.focus();
   }, []);
 
-  useEffect(() => {
 
+
+  useEffect(() => {
     if (!on) return;
-    
 
     const timer = setInterval(() => {
       setTime(old => {
+
         if (old <= 0) {
           sound();
           clearInterval(timer);
-          setTime(300);
+
+          if (flag) {
+            setTime(1500);
+            setFlag(false);
+            setOn(true);
+          } else {
+            setTime(300)
+            setFlag(true)
+            setOn(true);
+          }
           setOn(false);
           return 0;
         }
-
         return old - 1;
-
       })
     }, 1000);
 
@@ -57,29 +66,26 @@ export default function App() {
 
   return (
     <>
-      <div className="text-white flex flex-col justify-center items-center w-full h-screen">
+      <div className="font-mono text-white flex flex-col justify-center items-center sm:w-full w-[100%] h-screen">
 
-        <div className="border sm:px-12 sm:py-14 px-9 py-6 rounded-full border-5">
-          <div className="flex flex-row mt-15 mb-8">
-            <button ref={ref} onClick={() => { setTime(1500) }} className="focus:outline-none focus:ring-0 select-none border rounded-full py-1 px-3 hover:bg-neutral-700 focus:bg-neutral-600 border-2">pomodoro</button>
-            <button onClick={() => { setTime(300) }} className="select-none border rounded-full py-1 px-3 hover:bg-neutral-700 focus:bg-neutral-600 mx-4 border-2">short break</button>
-            <button onClick={() => { setTime(900) }} className="select-none border rounded-full py-1 px-3 hover:bg-neutral-700 focus:bg-neutral-600 border-2">long breack</button>
+        <div className="border sm:px-11 sm:py-14 px-6 py-2 rounded-full sm:border-5 border-4">
+
+          <div className="flex flex-row sm:mt-17 sm:mb-8 mt-20 mb-2">
+            <button ref={ref} onClick={() => { setTime(1500) }} className="text-sm focus:outline-none focus:ring-0 select-none border rounded-full sm:py-1 sm:px-3 py-1 px-1 hover:bg-neutral-700 focus:bg-neutral-600 border-2">pomodoro</button>
+            <button onClick={() => { setTime(300) }} className="text-sm select-none border rounded-full sm:py-3 sm:px-4 p hover:bg-neutral-700 focus:bg-neutral-600 sm:mx-7 mx-2 px-1 border-2">short break</button>
+            <button onClick={() => { setTime(900) }} className="text-sm select-none border rounded-full sm:py-1 sm:px-3 py-1 px-2 hover:bg-neutral-700 focus:bg-neutral-600 border-2">long break</button>
           </div>
-          <div className="flex flex-col justify-center items-center">
-            <label className="text-9xl">{formatTime(time)}</label>
+
+          <div className="flex flex-col justify-center w-full items-center text-center mt-5">
+            <label className="sm:text-9xl text-8xl">{formatTime(time)}</label>
             <button>
-              <img src={on ? "/assets/icons/pause.svg" : "/assets/icons/play.svg"} alt="test" className="w-20 h-20 focus:outline-none focus:ring-0 select-none"
+              <img src={on ? "/assets/icons/pause.svg" : "/assets/icons/play.svg"} alt="test" className="w-25 h-25 focus:outline-none focus:ring-0 select-none mt-2"
                 onClick={on ? clickPause : clickStart}
               />
             </button>
           </div>
-
         </div>
-
-
-
       </div>
-
     </>
   )
 }
